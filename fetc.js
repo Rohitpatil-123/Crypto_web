acc334b35605c021628d5d8c48968cc28da03b77
ROHIT SANJAY PATIL <99815954+Rohitpatil-123@users.noreply.github.com>
2022-08-18 21:05:59 +0530

    Add files via upload

---
+++ fetc.js
@@ -0,0 +1,24 @@
+fetch("https://api.nomics.com/v1/currencies/ticker?key=e7e8d3665fd7a6a32be426648c149ce50325fdcd").then((data)=>{
+    //console.log(data);
+    return data.json();
+}).then((objectData)=>{
+    console.log(objectData[0].symbol);
+
+    let tableData="";
+    objectData.map((values)=>{
+        tableData+=`
+        <tr>
+        <td>${values.rank}</td>
+        <td><img src="${values.logo_url}"/>&nbsp;&nbsp;${values.name}</td>
+        <td>${values.market_cap}</td>
+        <td>${values.price}</td>
+        
+      </tr>
+        
+        `;
+
+    });
+    document.getElementById("table_body").innerHTML=tableData;
+}).catch((err)=>{
+    console.log(err);
+})
